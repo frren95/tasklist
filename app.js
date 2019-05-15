899830b23affd485be25d9272e27f84f5a5b8a51
Frederique Renet <frederique.renet@free.fr>
2019-05-15 11:51:12 +0200

    First Commit

---
+++ app.js
@@ -0,0 +1,7 @@
+// DEfinie UI variables
+const form = document.querySelector('#task-form');
+const taskInput = document.querySelector('#task');
+const filter = document.querySelector('#filter') ;
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+
